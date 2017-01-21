ec1802af3a9c89c9934def6430c701ad0face1f5
Brynn Hawker <brynn@hawker.me>
2017-01-21 00:23:15 -0500

    Basic Tabs

--- components/pages/Teach.jsx
+++ components/pages/Teach.jsx
@@ -18,16 +18,30 @@ export default class Home extends Component {
   render() {
     return (
       <div className="teach">
-        <div className="container">
-          <div className="columns">
-            <div className="column col-12 text-center">
-              <a className="btn">Teaching! <Icon name="group" /></a><br /><br />
-              <a className="btn btn-link">Teaching! <Icon name="group" /></a><br /><br />
-              <a className="btn btn-primary">Teaching! <Icon name="group" /></a><br /><br />
-              <a className="btn btn-primary loading">Teaching! <Icon name="group" /></a>
-            </div>
-          </div>
+        <div className="sidebar">
+          <ul className="tab tab-side">
+            <li className="tab-item active">
+              <a href="#a">A</a>
+            </li>
+            <li className="tab-item">
+              <a href="#b">B</a>
+            </li>
+            <li className="tab-item">
+              <a href="#c">C</a>
+            </li>
+            <li className="tab-item">
+              <a href="#d">D</a>
+            </li>
+          </ul>
         </div>
+        <main className="view">
+          <div className="container text-center">
+            <a className="btn">Teaching! <Icon name="group" /></a><br /><br />
+            <a className="btn btn-link">Teaching! <Icon name="group" /></a><br /><br />
+            <a className="btn btn-primary">Teaching! <Icon name="group" /></a><br /><br />
+            <a className="btn btn-primary loading">Teaching! <Icon name="group" /></a>
+          </div>
+        </main>
       </div>
     );
   }
