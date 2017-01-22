dc7d3ab9e40a3f908f410afdafa59beee2a0db7b
Jake Cooper <jake.elijah.cooper@gmail.com>
2017-01-21 22:56:57 -0500

    Add get initials in the right spot

--- components/pages/Teach.jsx
+++ components/pages/Teach.jsx
@@ -36,6 +36,17 @@ export default class Home extends Component {
     };
   }
 
+  getInitials(nameString) {
+    const firstLast = nameString.split(' ');
+    let initials;
+    if (firstLast.length === 1) {
+      initials = firstLast[0][0];
+    } else {
+      initials = `${firstLast[0].charAt(0)}${firstLast[firstLast.length - 1].charAt(0)}`;
+    }
+    return initials;
+  }
+
   getUsers(roomName) {
     this.ref.syncState(`rooms/${roomName}/users`, {
       context: this,
@@ -74,20 +85,6 @@ export default class Home extends Component {
     this.setState({
       activeStudentIndex: index,
     });
-  }
-
-  // static defaultProps = {
-  // }
-
-  getInitials(nameString) {
-    const firstLast = nameString.split(' ');
-    let initials;
-    if (firstLast.length === 1) {
-      initials = firstLast[0][0];
-    } else {
-      initials = `${firstLast[0].charAt(0)}${firstLast[firstLast.length - 1].charAt(0)}`;
-    }
-    return initials;
   }
 
   render() {
