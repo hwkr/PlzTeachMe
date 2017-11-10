f49d8856938f30f6e08f288f338f2cc8f2d07eeb
Travis CI <travis.ci.build@gmail.com>
2017-11-10 19:08:20 +0000

    Travis CI - Deployed b203d40 with Webpack

--- dist/page.js
+++ dist/page.js
@@ -3,15 +3,13 @@
 /* eslint-disable */
 (function (l) {
   if (l.search) {
-    (function () {
-      var q = {};
-      l.search.slice(1).split('&').forEach(function (v) {
-        var a = v.split('=');
-        q[a[0]] = a.slice(1).join('=').replace(/~and~/g, '&');
-      });
-      if (q.p !== undefined) {
-        window.history.replaceState(null, null, l.pathname.slice(0, -1) + (q.p || '') + (q.q ? '?' + q.q : '') + l.hash);
-      }
-    })();
+    var q = {};
+    l.search.slice(1).split('&').forEach(function (v) {
+      var a = v.split('=');
+      q[a[0]] = a.slice(1).join('=').replace(/~and~/g, '&');
+    });
+    if (q.p !== undefined) {
+      window.history.replaceState(null, null, l.pathname.slice(0, -1) + (q.p || '') + (q.q ? '?' + q.q : '') + l.hash);
+    }
   }
 })(window.location);
